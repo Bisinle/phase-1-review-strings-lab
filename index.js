f2cd988a1ce2369444c49b0c8eb8673422af4aba
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-12 12:34:37 +0300

    strings lab DONE

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+let  welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+let excitedWelcomeMessage ="WELCOME TO FLATBOOK, "
+excitedWelcomeMessage = `"WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}"!`
+
+
+
+let shortGreeting = "Welcome, "
+shortGreeting = `Welcome, "${currentUser.charAt(0)}!`
